f2c0f6c15602aa086c48a47ea25d5b53496b18c8
人間工作 <coolxiaom95@gmail.com>
2024-12-14 17:00:39 +0800

    get offline players in a fast way

--- src/main/java/top/mrxiaom/figura/authprovider/PluginMain.java
+++ src/main/java/top/mrxiaom/figura/authprovider/PluginMain.java
@@ -1,10 +1,15 @@
 package top.mrxiaom.figura.authprovider;
 
 import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.HandlerList;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.Nullable;
 import top.mrxiaom.figura.authprovider.auth.AuthMeProvider;
 import top.mrxiaom.figura.authprovider.auth.IAuthProvider;
 import top.mrxiaom.figura.authprovider.server.HttpAdapter;
@@ -14,11 +19,14 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.lang.reflect.Method;
+import java.util.Map;
+import java.util.TreeMap;
 import java.util.logging.Logger;
 
-public class PluginMain extends JavaPlugin {
+public class PluginMain extends JavaPlugin implements Listener {
     HttpAdapter adapter = null;
     IAuthProvider authProvider = null;
+    private static Map<String, OfflinePlayer> playersByName = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
     @Override
     public void onEnable() {
         Bukkit.getMessenger().registerOutgoingPluginChannel(this, "figura:reconnect");
@@ -30,7 +38,23 @@ public class PluginMain extends JavaPlugin {
         }
         Commands.register(this);
         reloadConfig();
+        Bukkit.getPluginManager().registerEvents(this, this);
+        Bukkit.getScheduler().runTaskAsynchronously(this, () -> {
+            for (OfflinePlayer player : Bukkit.getOfflinePlayers()) {
+                if (player != null) {
+                    String name = player.getName();
+                    if (name != null) {
+                        playersByName.put(name, player);
+                    }
+                }
+            }
+        });
+    }
 
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent e) {
+        Player player = e.getPlayer();
+        playersByName.put(player.getName(), player);
     }
 
     @Override
@@ -90,11 +114,17 @@ public class PluginMain extends JavaPlugin {
         }
         logger.warning(sw.toString());
     }
+    @Nullable
     public static Player getOnlinePlayer(String name) {
         if (name == null || name.isEmpty()) return null;
         for (Player player : Bukkit.getOnlinePlayers()) {
             if (player.getName().equals(name)) return player;
         }
         return null;
+    }
+
+    @Nullable
+    public static OfflinePlayer getOfflinePlayer(String name) {
+        return playersByName.get(name);
     }
 }
